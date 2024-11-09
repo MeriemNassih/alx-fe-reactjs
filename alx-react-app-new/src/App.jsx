ad45ca3aecf007dc323b0d89ffde51b88d19900d
Meriem Nassih <nassihmariam2@gmail.com>
2024-11-09 22:37:25 +0100

    Folder renaming and file changes

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -7,6 +7,7 @@ import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
 import UserProfile from './components/UserProfile';
+import Counter from './components/Counter';
 
 
 function App() {
@@ -18,8 +19,9 @@ function App() {
         <Header />
             <MainContent />
             <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+            <Counter />
             <Footer />
-        
+            
     </div>
 )
 }
