ea1ea8a322aed00d8fed1fbb44b2162d539391b7
Meriem Nassih <nassihmariam2@gmail.com>
2024-11-10 20:25:16 +0100

    ....

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -8,7 +8,7 @@ import MainContent from './components/MainContent';
 import Footer from './components/Footer';
 import UserProfile from './components/UserProfile';
 import Counter from './components/Counter';
-import { UserContext } from './UserContext';  
+import { UserContext } from './components/UserContext';  
 
 function App() {
   const [count, setCount] = useState(0);
