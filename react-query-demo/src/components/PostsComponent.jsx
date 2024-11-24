67a2fbc8fef9d00fb0d70c8311bcd932c53adcd7
Meriem Nassih <nassihmariam2@gmail.com>
2024-11-24 22:16:22 +0100

    modif

--- react-query-demo/src/components/PostsComponent.jsx
+++ react-query-demo/src/components/PostsComponent.jsx
@@ -12,10 +12,12 @@ const fetchPosts = async () => {
 };
 
 const PostsComponent = () => {
-  // Utilisation de useQuery avec les options de mise en cache
+  // Utilisation de useQuery avec les options de mise en cache et actualisation sur focus de fenêtre
   const { data, error, isLoading, isFetching, refetch, isError } = useQuery('posts', fetchPosts, {
     staleTime: 60000, // Données considérées comme fraîches pendant 1 minute (60 000 ms)
     keepPreviousData: true, // Garder les données précédentes pendant que les nouvelles données sont récupérées
+    cacheTime: 300000, // Garder les données en cache pendant 5 minutes (300 000 ms)
+    refetchOnWindowFocus: true, // Refetch des données lorsque la fenêtre reprend le focus
   });
 
   if (isLoading) {
