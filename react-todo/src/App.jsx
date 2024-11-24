af13db901b42a00f7297832416b68c950bc32bb4
Meriem Nassih <nassihmariam2@gmail.com>
2024-11-24 21:53:18 +0100

    modif

--- react-todo/src/App.jsx
+++ react-todo/src/App.jsx
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+// Importation de React (bien que cela ne soit pas nécessaire dans les versions récentes de React, c'est toujours une bonne pratique)
+import React from 'react';
+
+// Importation du composant TodoList
+import TodoList from './components/TodoList';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Welcome to the Todo List App</h1>
+      {/* Utilisation du composant TodoList */}
+      <TodoList />
     </div>
   );
 }
