269185d6ee40fe5a1de475136759937a61d5e40a
Meriem Nassih <nassihmariam2@gmail.com>
2024-11-30 19:11:11 +0100

    up

--- tailwind-react-integration/tailwind.config.js
+++ tailwind-react-integration/tailwind.config.js
@@ -1,7 +1,13 @@
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Chemins pour vos fichiers React
+  ],
   theme: {
     extend: {},
+  },
+  darkMode: 'media', // Utilise 'media' ou 'class'
+  variants: {
+    extend: {}, // Étendez les variantes si nécessaire
   },
   plugins: [],
 };
