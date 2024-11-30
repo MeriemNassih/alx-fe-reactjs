9dd2dac2be891151dbf06f156cf0d040db45b232
Meriem Nassih <nassihmariam2@gmail.com>
2024-11-30 18:11:13 +0100

    Initialisation du projet avec Tailwind CSS

---
+++ tailwind-react-integration/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  darkMode: false, // Vous pouvez utiliser 'media' ou 'class' pour activer le mode sombre
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
