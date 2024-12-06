e67a96ab9effbed14879d37d79ac3e9c413668ef
Meriem Nassih <nassihmariam2@gmail.com>
2024-12-06 19:14:51 +0100

    uup

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import data from './data.json'; // Assurez-vous que le chemin est correct
+
 
 function HomePage() {
   return (
